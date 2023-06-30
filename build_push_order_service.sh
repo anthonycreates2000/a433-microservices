4bea9e769236aa2b515a44dd15d1d5a3cc062563
maxzx3000 <anthonyoktavius1110@gmail.com>
2023-06-30 17:28:24 +0700

    Added build push image order service in shell.

---
+++ build_push_order_service.sh
@@ -0,0 +1,15 @@
+# Build a docker image from dockerfile in this directory.
+docker build -t order-service:v1 .
+
+# List all images that are installed in docker.
+docker images
+
+# Rename docker image's tag, so it can be compatible with github packages' tag.
+# The format for github docker image tag is: ghcr.io/<username>/<docker_image_name>:<docker_image_tag>.
+docker tag order-service:v1 ghcr.io/anthonycreates2000/order-service:v1
+
+# Get the password from environment variable and login to the github package account.
+echo $PASSWORD_GITHUB_PACKAGES | docker login ghcr.io -u anthonycreates2000 --password-stdin
+
+# Push built image to github package.
+docker push ghcr.io/anthonycreates2000/order-service:v1
