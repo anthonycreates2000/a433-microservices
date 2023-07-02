225022ce8862f9c2ab086b41e2e89569ccd142b7
anthonycreates2000 <anthonyoktavius1110@gmail.com>
2023-07-02 12:15:10 +0700

    Update kubernetes-rabbitmq-operator.sh's in helm package and expose

--- kubernetes-rabbitmq-operator.sh
+++ kubernetes-rabbitmq-operator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Get helm data.
+# Get helm package.
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 ./get_helm.sh
 ./get_helm.sh
@@ -11,10 +11,10 @@ kubectl create namespace rabbit
 # Install rabbitmq to rabbit namespace, with username and password from environment variables.
 helm install mu-rabbit stable/rabbitmq --set rabbitmq.username=$RABBITMQ_USERNAME,rabbitmq.password=$RABBITMQ_PASSWORD --namespace rabbit
 
-# For RabbitMQ Management UI Service.
+# Expose mu-rabbit-rabbitmq to access RabbitMQ Management UI.
 kubectl expose service mu-rabbit-rabbitmq --type=NodePort --target-port=15672 --name=mu-rabbit-rabbitmq-ext --namespace rabbit
 # kubectl expose service mu-rabbit-rabbitmq-headless --type=NodePort --target-port=15672 --name=mu-rabbit-mq-headless-ext --namespace rabbit
 
-# For queuing service.
+# Expose mu-rabbit-rabbitmq to access queuing service.
 kubectl expose service mu-rabbit-rabbitmq --type=NodePort --target-port=5672 --name=mu-rabbit-rabbitmq-amqp-ext --namespace rabbit
 # kubectl expose service mu-rabbit-rabbitmq-headless --type=NodePort --target-port=5672 --name=mu-rabbit-mq-headless-queue-ext --namespace rabbit
