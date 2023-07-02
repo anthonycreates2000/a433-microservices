49e3b6bff2105cfb16ec412bfe066c51919f0cb7
anthonycreates2000 <anthonyoktavius1110@gmail.com>
2023-07-02 12:07:28 +0700

    Added code to get ip address in ingress to access istio server.

---
+++ get_ip_address_ingress_istio.sh
@@ -0,0 +1,14 @@
+# ==================================
+# IMPORTANT NOTE: Make sure to run minikube tunnel first from .
+# ==================================
+# Get all inggress IP address parts to access istio external IP address.
+export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].port}')
+export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].port}')
+
+# Get the Address and Port from Ingress to access Istio.
+echo "$INGRESS_HOST" "$INGRESS_PORT" "$SECURE_INGRESS_PORT"
+
+# Make a full IP Address for accessing our application.
+export GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
+echo "$GATEWAY_URL"
