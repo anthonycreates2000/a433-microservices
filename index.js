263f9a77bf22443ee15161e24f8804c6c4ae8c76
maxzx3000 <anthonyoktavius1110@gmail.com>
2023-07-02 11:57:11 +0700

    Added comments in index.js.

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
+// Dotenv is a dependency for loading environment variables from a .env file into process.env
 require('dotenv').config()
 
+// Set express as the server for the app. 
 const express = require("express");
 const app = express();
 
+// Body-parser module parses the JSON, buffer, string and URL encoded data submitted using HTTP POST request.
 const bp = require("body-parser");
 app.use(bp.json());
 
+// Initialize amqplib for initializing RabbitMQ.
+// Then, we define the URL from kubernetes' environment variable.
 const amqp = require("amqplib");
 const amqpServer = process.env.AMQP_URL;
 var channel, connection;
 
+// After the app is initialized, try connecting to RabbitMQ's messaging queue.
 connectToQueue();
 
+// Set the function to connect to RabbitMQ's messaging queue.
+// If I can connect, then we print the message "Connected to the queue!".
 async function connectToQueue() {
     connection = await amqp.connect(amqpServer);
     channel = await connection.createChannel();
@@ -24,12 +32,15 @@ async function connectToQueue() {
     }
 }
 
+// If the URL path is "/order"...
+// Begin creating the order message to RabbitMQ's message queue.
 app.post("/order", (req, res) => {
     const { order } = req.body;
     createOrder(order);
     res.send(order);
 });
 
+// Set the function for init message to RabbitMQ's message queue.
 const createOrder = async order => {
     const queue = "order";
     await channel.sendToQueue(queue, Buffer.from(JSON.stringify(order)));
@@ -42,6 +53,8 @@ const createOrder = async order => {
     });
 };
 
+// Make this app available to be accessed within the specified port in environment variable
+// in kubernetes.
 app.listen(process.env.PORT, () => {
     console.log(`Server running at ${process.env.PORT}`);
 });
